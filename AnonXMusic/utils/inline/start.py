cda66336d392658bcab484e57da70fd9aac177f4
Tosu <167279662+about-tosu@users.noreply.github.com>
2025-01-31 17:25:22 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_0"], url=f"https://t.me/Cloning_music_Bot"),
         ],
     ]
     return buttons
